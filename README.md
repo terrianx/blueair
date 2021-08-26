f744fb965514b6d873e954e705ca0a08c22982ed
88253868 <txiaopk@gmail.com>
2021-08-25 21:20:40 -0400

    Updated README and navbar features

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Project Status: active
 
 Name: BlueAir Website
 
+Author: https://github.com/briancodex
+
 Intro: A single page website for a mock company that sells air. 
 
-Background: A website created to practice html, css, and js and learn new frontend skills. Began in July 2021.
+Background: A website created to practice HTML, CSS, and JS and learn new frontend skills. Began in July 2021. 
--- animate.js
+++ animate.js
@@ -2,7 +2,22 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
 const navLogo = document.querySelector('#navbar-logo');
 
+// show mobile menu
 const displayMobile = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 };
+
+// close mobile menu
+const hideMobile = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 800 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+}
+
+menu.addEventListener('click', displayMobile);
+
+menuLinks.addEventListener('click', hideMobile);
+navLogo.addEventListener('click', hideMobile);
