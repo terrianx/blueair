febc8c2f7c9f79cf73618b7eb908594fb1f38f81
ytx <txiaopk@gmail.com>
2021-10-23 15:07:29 -0400

    Improved nav responsiveness

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 // ==============================
 // === js file for index.hmtl ===
 // ==============================
+
+// changes color of nav when scrolling past intro
+const nav = document.querySelector("nav");
+const section = document.getElementById("logo");
+
+const sectionOptions = {
+    rootMargin: "-200px"
+};
+
+const sectionObserver = new IntersectionObserver
+    (function(entries, sectionObserver) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                nav.classList.add("nav-scrolled");
+            } else {
+                nav.classList.remove("nav-scrolled");
+            }
+        });
+    }, sectionOptions);
+
+sectionObserver.observe(section);
